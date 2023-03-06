22f527c86321439af5468c7568367a05de693d04
Ana Maria Mihalceanu <mihalceanuana@gmail.com>
2023-03-06 16:16:52 +0100

    Update TodoItem.java

--- todo-app-springboot/src/main/java/org/acme/example/model/TodoItem.java
+++ todo-app-springboot/src/main/java/org/acme/example/model/TodoItem.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package org.acme.example.model;
 import javax.persistence.Id;
 import javax.persistence.Entity;
